44f85b80063651526efca9db7a93e54b8f0650b8
712696307@qq.com <712696307@qq.com>
2019-04-09 19:20:22 +0800

    2019年04月09日19:20:05

---
+++ Promise.js
@@ -0,0 +1,221 @@
+/**
+ * 
+ * 
+ */
+
+// class Promise(executor) {
+// }
+
+
+function Promise(executor) {
+  var self = this
+
+  self.status = 'pending'
+  self.onResolvedCallback = []
+  self.onRejectedCallback = []
+
+  function resolve(value) {
+    if (value instanceof Promise) {
+      return value.then(resolve, reject)
+    }
+    setTimeout(function() { // 异步执行所有的回调函数
+      if (self.status === 'pending') {
+        self.status = 'resolved'
+        self.data = value
+        for (var i = 0; i < self.onResolvedCallback.length; i++) {
+          self.onResolvedCallback[i](value)
+        }
+      }
+    })
+  }
+
+  function reject(reason) {
+    setTimeout(function() { // 异步执行所有的回调函数
+      if (self.status === 'pending') {
+        self.status = 'rejected'
+        self.data = reason
+        for (var i = 0; i < self.onRejectedCallback.length; i++) {
+          self.onRejectedCallback[i](reason)
+        }
+      }
+    })
+  }
+
+  try {
+    executor(resolve, reject)
+  } catch (reason) {
+    reject(reason)
+  }
+}
+
+//https://promisesaplus.com/#point-47  可以混合原生Promise 使用     2.3
+function resolvePromise(promise2, x, resolve, reject) {
+  var then
+  var thenCalledOrThrow = false
+
+  if (promise2 === x) { // 对应标准2.3.1节
+    return reject(new TypeError('Chaining cycle detected for promise!'))
+  }
+
+  if (x instanceof Promise) { // 对应标准2.3.2节
+    // 如果x的状态还没有确定，那么它是有可能被一个thenable决定最终状态和值的
+    // 所以这里需要做一下处理，而不能一概的以为它会被一个“正常”的值resolve
+    if (x.status === 'pending') {
+      x.then(function(value) {
+        resolvePromise(promise2, value, resolve, reject)
+      }, reject)
+    } else { // 但如果这个Promise的状态已经确定了，那么它肯定有一个“正常”的值，而不是一个thenable，所以这里直接取它的状态
+      x.then(resolve, reject)
+    }
+    return
+  }
+
+  if ((x !== null) && ((typeof x === 'object') || (typeof x === 'function'))) { // 2.3.3
+    try {
+
+      // 2.3.3.1 因为x.then有可能是一个getter，这种情况下多次读取就有可能产生副作用
+      // 即要判断它的类型，又要调用它，这就是两次读取
+      then = x.then
+      if (typeof then === 'function') { // 2.3.3.3
+        then.call(x, function rs(y) { // 2.3.3.3.1
+          if (thenCalledOrThrow) return // 2.3.3.3.3 即这三处谁选执行就以谁的结果为准
+          thenCalledOrThrow = true
+          return resolvePromise(promise2, y, resolve, reject) // 2.3.3.3.1
+        }, function rj(r) { // 2.3.3.3.2
+          if (thenCalledOrThrow) return // 2.3.3.3.3 即这三处谁选执行就以谁的结果为准
+          thenCalledOrThrow = true
+          return reject(r)
+        })
+      } else { // 2.3.3.4
+        resolve(x)
+      }
+    } catch (e) { // 2.3.3.2
+      if (thenCalledOrThrow) return // 2.3.3.3.3 即这三处谁选执行就以谁的结果为准
+      thenCalledOrThrow = true
+      return reject(e)
+    }
+  } else { // 2.3.4
+    resolve(x)
+  }
+}
+
+Promise.prototype.then = function(onResolved, onRejected) {
+  let self = this
+  let promise2
+  //promise 值的穿透 。 
+  onResolved = typeof onResolved === 'function' ? onResolved : function(v) {
+    return v
+  }
+  onRejected = typeof onRejected === 'function' ? onRejected : function(r) {
+    throw r
+  }
+
+  if (self.status === 'resolved') {
+    return promise2 = new Promise(function(resolve, reject) {
+      setTimeout(function() { // 异步执行onResolved
+        try {
+          var x = onResolved(self.data)
+          resolvePromise(promise2, x, resolve, reject)
+        } catch (reason) {
+          reject(reason)
+        }
+      })
+    })
+  }
+
+  if (self.status === 'rejected') {
+    return promise2 = new Promise(function(resolve, reject) {
+      setTimeout(function() { // 异步执行onRejected
+        try {
+          var x = onRejected(self.data)
+          resolvePromise(promise2, x, resolve, reject)
+        } catch (reason) {
+          reject(reason)
+        }
+      })
+    })
+  }
+
+  if (self.status === 'pending') {
+    // 这里之所以没有异步执行，是因为这些函数必然会被resolve或reject调用，而resolve或reject函数里的内容已是异步执行，构造函数里的定义
+    return promise2 = new Promise(function(resolve, reject) {
+      self.onResolvedCallback.push(function(value) {
+        try {
+          var x = onResolved(value)
+          resolvePromise(promise2, x, resolve, reject)
+        } catch (r) {
+          reject(r)
+        }
+      })
+
+      self.onRejectedCallback.push(function(reason) {
+        try {
+          var x = onRejected(reason)
+          resolvePromise(promise2, x, resolve, reject)
+        } catch (r) {
+          reject(r)
+        }
+      })
+    })
+  }
+}
+
+Promise.prototype.catch = function(onRejected) {
+  return this.then(null, onRejected)
+}
+Promise.all = function(promises) {
+  return new Promise(function(resolve, reject) {
+    let resolvedCounter = 0
+    let promiseNum = promises.length
+    let resolvedValues = new Array(promiseNum)
+
+    for (var i = 0; i < promiseNum; i++) {
+      (function(i) {
+        Promise.resolve(promises[i]).then(function(value) {
+          resolvedCounter++
+          resolvedValues[i] = value
+          if (resolvedCounter === promiseNum) {
+            return resolve(resolvedValues)
+          }
+        }, function(reason) {
+          reject(reason)
+        })
+      })(i)
+    }
+  })
+}
+Promise.race = function(promises) {
+  return new Promise(function(resolve, reject) {
+    for (var i = 0, i = promises.length; i < len; i++) {
+      Promise.resolve(promises[i]).then(function(value) {
+        return resolve(value)
+      }, function(reason) {
+        return reject(reason)
+      })
+    }
+  })
+}
+Promise.resolve = function(value) {
+  return new Promise(function(resolve, reject) {
+    resolvePromise(Promise, value, resolve, reject)
+  })
+}
+Promise.reject = function(reason) {
+  return new Promise(function(resolve, reject) {
+    reject()
+  })
+}
+
+
+Promise.deferred = function() {
+  var dfd = {}
+  dfd.promise = new Promise(function(resolve, reject) {
+    dfd.resolve = resolve
+    dfd.reject = reject
+  })
+  return dfd
+}
+
+try {
+  module.exports = Promise
+} catch (e) {}
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # my-promise
+
+---
+
+实现符合Promises/A+ 标准的 promise 一枚 
+
+---
+### 实现的原生api
+
+- [x] all
+- [x] race
+- [x] resolve
+- [x] reject
+
+### 辅助性api
+
+- [ ] finally
+- [ ] spread
+- [ ] delay
+- [ ] done
